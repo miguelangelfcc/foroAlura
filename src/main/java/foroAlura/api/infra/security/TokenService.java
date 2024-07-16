c92d5df23d6740381c73d0679ac3019621054546
miguelangelfcc <miguelcabelloc@gmail.com>
2024-07-15 23:43:38 -0400

    autenticacion y permisos

--- src/main/java/foroAlura/api/infra/security/TokenService.java
+++ src/main/java/foroAlura/api/infra/security/TokenService.java
@@ -4,6 +4,8 @@ package foroAlura.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import foroAlura.api.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,6 +33,32 @@ public class TokenService {
             throw new RuntimeException();
         }
 
+    }
+
+    public String getSubject(String token) {
+        if (token == null){
+            throw new RuntimeException();
+        }
+
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("topico api")
+                    // reusable verifier instance
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+
+
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
     }
 
     private Instant generarFechaExpiracion(){
